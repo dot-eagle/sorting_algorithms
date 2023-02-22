2c13cb293b4f32d0078b8305cbb88a23efcf936a
dot-eagle <lionvic3@gmail.com>
2023-02-22 12:47:07 -0800

    7. Merge sort: Write a function that sorts an array of integers in ascending order using the Merge sort algorithm

---
+++ 103-merge_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ * using the Merge sort algorithm
+ * @array: list if elements
+ * @size: of array
+ *
+ * Return: void, nothing
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	(void) array;
+	(void) size;
+}
