8cb355fda2f649973b351da365baaedfb42382ef
dot-eagle <lionvic3@gmail.com>
2023-02-22 13:04:06 -0800

    8. Heap sort: Write a function that sorts an array of integers in ascending order using the Heap sort algorithm

---
+++ 104-heap_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * heap_sort - sorts an array of integers in ascending order
+ * using the Heap sort algorithm
+ * @array: of elements to swap
+ *
+ * Return: void, nothing
+ */
+
+void heap_sort(int *array, size_t size)
+{
+	(void) array;
+	(void) size;
+}
